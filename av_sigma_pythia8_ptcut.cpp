1920b719b2f9d6797f2692fc5eea7b3b70f0482d
aontoso <albasotoontoso@10-4-19-47.user.vpn.bnl.local>
2019-03-14 15:50:51 -0400

    Very slow version

---
+++ av_sigma_pythia8_ptcut.cpp
@@ -0,0 +1,8 @@
+//
+//  av_sigma_pythia8_ptcut.cpp
+//  
+//
+//  Created by Alba Soto Ontoso on 2/11/19.
+//
+
+#include <stdio.h>
---
+++ bkg2_av_sigma_pythia8.cpp
@@ -0,0 +1,8 @@
+//
+//  bkg2_av_sigma_pythia8.cpp
+//  
+//
+//  Created by Alba Soto Ontoso on 2/12/19.
+//
+
+#include <stdio.h>
